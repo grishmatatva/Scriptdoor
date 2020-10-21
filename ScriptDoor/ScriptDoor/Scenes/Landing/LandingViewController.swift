1d0b4b0a3428d7a8d27c192470a41b135a703a9d
grishmatatva <grishma.tatva@gmail.com>
2020-10-21 22:19:33 +0530

    Landing Screen design

--- ScriptDoor/ScriptDoor/Scenes/Landing/LandingViewController.swift
+++ ScriptDoor/ScriptDoor/Scenes/Landing/LandingViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class LandingViewController: UIViewController {
 
+    // MARK: - Outlet
+    @IBOutlet private weak var textSearch: UITextField!
+    @IBOutlet private weak var profileConfirm : NSLayoutConstraint!
+    @IBOutlet private weak var btnDoItLater: UIButton!
+    @IBOutlet private weak var btnClickToConfirm: UIButton!
+    
+    // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    // MARK: - Action Methods
+    @IBAction func didTapOnDoItLater() {
+        self.profileConfirm.constant = 0
+        btnDoItLater.isHidden = true
+        btnClickToConfirm.isHidden = true
+    }
 }
